7938666c2434a89b0822101e3037276dcf312261
s-vashistha <saumyavashistha.27@gmail.com>
2024-09-19 17:10:36 +0530

    path change

--- backend/routes/PostData.js
+++ backend/routes/PostData.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const EspData = require('./models/EspData'); // Import the Sequelize models
+
+const EspData = require('../models/EspData');
+// Import the Sequelize models
 
 // POST route for device data
 router.post('/data', async (req, res) => {
