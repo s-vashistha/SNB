47f9f33951c56e5fe863bf2de7db2ad893285f81
s-vashistha <saumyavashistha.27@gmail.com>
2024-09-21 11:06:08 +0530

    db connection handling

--- backend/db.js
+++ backend/db.js
@@ -15,7 +15,32 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
       rejectUnauthorized: false // SSL settings for Render
     }
   },
-  logging: false // Optional: disable logging of queries
+  logging: false,// Optional: disable logging of queries
+  pool: {                   //check pool attributes
+    max:10,
+    min: 7,
+    acquire: 50000,
+    idle: 70000
+  }
+});
+
+
+// Authenticate and handle connection events
+sequelize.authenticate().then(() => {
+  console.log('Connection established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+// Handle database disconnection and attempt to reconnect
+sequelize.connectionManager.on('error', err => {
+  console.error('Database connection lost:', err);
+  sequelize.authenticate().then(() => {
+    console.log('Reconnected successfully.');
+  }).catch(err => {
+    console.error('Reconnection failed:', err);
+  });
 });
 
 module.exports = sequelize;
+
